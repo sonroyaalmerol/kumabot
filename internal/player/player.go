2afdad71270876ddf9ab803c34bc757750b2bd62
Son Roy Almerol <github@snry.me>
2025-09-04 00:55:24 -0400

    fix revert

--- internal/player/player.go
+++ internal/player/player.go
@@ -211,7 +211,11 @@ func (p *Player) Play(ctx context.Context, s *discordgo.Session) error {
 			return err
 		}
 
-		inputURL = stream.PickDirectURL(info)
+		mu := stream.PickMediaURL(info)
+		if mu.URL == "" {
+			return errors.New("no usable media URL (direct or HLS)")
+		}
+		inputURL = mu.URL
 	}
 
 	// Build a playback-scoped context so we can cancel from Pause/Stop/etc.
